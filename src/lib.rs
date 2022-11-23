0de36521e8804c9d1bfe8407fb65c996c1555736
Ahmed Radwan <aradwann@proton.me>
2022-11-23 21:39:14 +0200

    use iterators in config build and search functions

--- src/lib.rs
+++ src/lib.rs
@@ -8,18 +8,23 @@ pub struct Config {
 }
 
 impl Config {
-    pub fn build(args: &[String]) -> Result<Config, &'static str> {
-        if args.len() < 3 {
-            return Err("not enough arguments");
-        }
-        let query = args[1].clone();
-        let file_path = args[2].clone();
+    pub fn build(mut args: impl Iterator<Item = String>) -> Result<Config, &'static str> {
+        args.next(); // skip the first arg which is the *name of the program*
+
+        let query = match args.next() {
+            Some(arg) => arg,
+            None => return Err("Didn't get a query String"),
+        };
+        let file_path = match args.next() {
+            Some(arg) => arg,
+            None => return Err("Didn't get a file path"),
+        };
         let ignore_case = env::var("IGNORE_CASE").is_ok();
-        return Ok(Config {
+        Ok(Config {
             query,
             file_path,
             ignore_case,
-        });
+        })
     }
 }
 pub fn run(config: Config) -> Result<(), Box<dyn Error>> {
@@ -36,25 +41,17 @@ pub fn run(config: Config) -> Result<(), Box<dyn Error>> {
 }
 
 pub fn search<'a>(query: &str, contents: &'a str) -> Vec<&'a str> {
-    let mut result = Vec::new();
-    for line in contents.lines() {
-        if line.contains(query) {
-            result.push(line);
-        }
-    }
-    result
+    contents
+        .lines()
+        .filter(|line| line.contains(query))
+        .collect()
 }
 
 pub fn search_case_insesitive<'a>(query: &str, contents: &'a str) -> Vec<&'a str> {
-    let query = query.to_lowercase();
-    let mut result = Vec::new();
-
-    for line in contents.lines() {
-        if line.to_lowercase().contains(&query) {
-            result.push(line);
-        }
-    }
-    result
+    contents
+        .lines()
+        .filter(|line| line.to_lowercase().contains(&query.to_lowercase()))
+        .collect()
 }
 
 #[cfg(test)]
--- src/main.rs
+++ src/main.rs
@@ -4,8 +4,8 @@ use minigrep::Config;
 use std::{env, process};
 
 fn main() {
-    let args: Vec<String> = env::args().collect();
-    let config = Config::build(&args).unwrap_or_else(|err| {
+    // let args: Vec<String> = env::args().collect();
+    let config = Config::build(env::args()).unwrap_or_else(|err| {
         eprintln!("problem parsing arguments {err}");
         process::exit(1);
     });
